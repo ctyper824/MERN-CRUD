ed6f978e4c0f9b9912e161ee9c9c89cc7213e373
ctyper824 <ctyper824@gmail.com>
2025-03-04 09:29:58 -0800

    removed the process of setting PORT :hammer:

--- server/index.js
+++ server/index.js
@@ -63,7 +63,7 @@ app.use((req, res) => {
 });
 
 /* MONGOOSE SETUP*/
-const PORT = process.env.PORT || 6001;
+// const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
